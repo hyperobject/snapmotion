490e017117d13b6332704c7209e9acd9add3bb03
Technoboy10 <cchudson98@gmail.com>
2013-06-30 15:48:55 -0600

    implement yaw, pitch, and roll for hands

--- snapmotion.py
+++ snapmotion.py
@@ -116,6 +116,29 @@ class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
             return f
+        elif 'handypr' in path: 
+            regex = re.compile('/handypr([ypr])([1|2])')
+            m = regex.match(path)
+            hand = int(m.group(2)) - 1
+            f = open(ospath + '/return', 'w+')
+            if m.group(1) == 'y':
+                f.write(str(frame.hands[hand].palm_position.yaw))
+            if m.group(1) == 'p':
+                f.write(str(frame.hands[hand].palm_position.pitch))
+            if m.group(1) == 'r':
+                f.write(str(frame.hands[hand].palm_position.roll))
+            f.close()
+            f = open(ospath + '/return', 'rb')
+            ctype = self.guess_type(ospath + '/return')
+            self.send_response(200)
+            self.send_header("Content-type", ctype)
+            fs = os.fstat(f.fileno())
+            self.send_header("Content-Length", str(fs[6]))
+            self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
+            return f
+        
 if __name__ == "__main__":
     import os
     import re
